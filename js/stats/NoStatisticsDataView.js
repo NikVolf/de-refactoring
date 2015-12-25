15de1bf75417e91fcb0f44bf37b9b09c9d24f441
Nikolay Volf <nikvolf@gmail.com>
2015-12-25 18:40:55 +0300

    removed base code

--- js/stats/NoStatisticsDataView.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by nvolf on 30.04.2015.
- */
-
-define(['text!./tpl/nodata.html'], function(template) {
-    return Backbone.Marionette.ItemView.extend({
-        template: Handlebars.compile(template)
-    });
-});
